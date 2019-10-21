98af48b4732d91a10e9fb7c2ce08ff8c6b95b87a
Chuyang Deng <chuyangd@amazon.com>
2019-10-21 12:15:48 -0700

    fix typo

--- sagemaker-spark-sdk/src/test/scala/com/amazonaws/services/sagemaker/sparksdk/algorithms/PCASageMakerEstimatorTests.scala
+++ sagemaker-spark-sdk/src/test/scala/com/amazonaws/services/sagemaker/sparksdk/algorithms/PCASageMakerEstimatorTests.scala
@@ -106,7 +106,7 @@ class PCASageMakerEstimatorTests extends FlatSpec with MockitoSugar {
       "749696950732.dkr.ecr.eu-west-3.amazonaws.com/pca:1")
 
     val estimatorMESouth1 = createPCAEstimator(region = Regions.ME_SOUTH_1.getName)
-    assert(estimatorEUWest3.trainingImage ==
+    assert(estimatorMESouth1.trainingImage ==
       "249704162688.dkr.ecr.me-south-1.amazonaws.com/pca:1")
   }
 
